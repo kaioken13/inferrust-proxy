bbca04c2a008e3bcfad54738baf2146b3941fbea
Thiago Aragao <thiaragao147@gmail.com>
2026-08-10 00:17:24 -0300

    First commit

--- inferrust-proxy/src/main.rs
+++ inferrust-proxy/src/main.rs
@@ -1,3 +1,6 @@
+fn main() {
+    println!("Hello, world!");
+}
 use axum::{
     routing::{get, post},
     Json, Router,
---
+++ inferrust-proxy/target/debug/deps/inferrust_proxy-ecde1994d48e2815.d
@@ -0,0 +1,5 @@
+/home/thiaragao/projects/inferrust-proxy/target/debug/deps/inferrust_proxy-ecde1994d48e2815.d: src/main.rs
+
+/home/thiaragao/projects/inferrust-proxy/target/debug/deps/libinferrust_proxy-ecde1994d48e2815.rmeta: src/main.rs
+
+src/main.rs:
